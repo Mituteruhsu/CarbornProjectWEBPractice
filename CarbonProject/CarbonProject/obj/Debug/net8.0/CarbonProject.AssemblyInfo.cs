da9c11925add5f054d1b023d71be7e04bd55336d
Mituteruhsu <104893491+Mituteruhsu@users.noreply.github.com>
2025-10-27 12:22:22 +0800

    Update 註冊頁統編綁定問題修正

--- CarbonProject/CarbonProject/obj/Debug/net8.0/CarbonProject.AssemblyInfo.cs
+++ CarbonProject/CarbonProject/obj/Debug/net8.0/CarbonProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarbonProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26688f9632bc36d1e4ce2efbdfe4481a721a2443")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+145bb4eb68c2df3e980f3776792280e305c288d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarbonProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarbonProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
