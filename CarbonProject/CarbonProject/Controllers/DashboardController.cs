38eaf129e1a739e5d0068f6defb6411c793043c2
Mituteruhsu <104893491+Mituteruhsu@users.noreply.github.com>
2025-10-28 15:24:04 +0800

    Create Dashboard for users

---
+++ CarbonProject/CarbonProject/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using CarbonProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Text.Json;   // 用來轉 JSON 格式
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace CarbonProject.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- CarbonProject/CarbonProject/obj/Debug/net8.0/CarbonProject.AssemblyInfo.cs
+++ CarbonProject/CarbonProject/obj/Debug/net8.0/CarbonProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarbonProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbfa38d046b8688c5c0cd0b8f3e99defd615be5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5661363464752786b7a51aa90d3689b17d69ccb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarbonProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarbonProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
