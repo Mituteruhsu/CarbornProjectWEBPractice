47167954d7fe937c012f9b123825251ca5679bf1
Mituteruhsu <104893491+Mituteruhsu@users.noreply.github.com>
2025-11-07 20:24:33 +0800

    Create ProjectReport

--- CarbonProject/CarbonProject/obj/Debug/net8.0/CarbonProject.AssemblyInfo.cs
+++ CarbonProject/CarbonProject/obj/Debug/net8.0/CarbonProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarbonProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a14d0a3e0fafcc9b34b35f724bbe23cb12c52b7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfab865d8acb3f18d180ffdc6f2b7549d3be5a99")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarbonProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarbonProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
